65d334cdbcf1a475cee576c6df5add73ca4666b7
ZoeTrinh <thuminh10011997@gmail.com>
2023-01-30 16:50:53 +0700

    Lession 1: Hello Component

---
+++ src/hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return(
+      <div>
+        <h3 className='title'>Hello Component</h3>
+      </div>
+    )
+  }
+}
+
+export default Hello;
