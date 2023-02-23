148c5672a4fe88c38a0539f6513e0c4f96546513
ZoeTrinh <thuminh10011997@gmail.com>
2023-02-23 22:20:07 +0700

    javascript lession 2 end

--- js/javascript-lession-02/js/script.js
+++ js/javascript-lession-02/js/script.js
@@ -23,9 +23,11 @@
 // }
 
 // truy xuất dữ liệu
-// class: document.getElementsByClassName("Class")
-// id: document.getElementById("Id")
-// tag: document.getElementsByTagName("Tag")
+// class: document.getElementsByClassName("[Class]")
+// id: document.getElementById("[Id]")
+// tag: document.getElementsByTagName("[Tag]")
+// selector: document.querySelector("[data as CSS]")
+// selector: document.querySelectorAll("[data as CSS]")
 
 // var h3 = document.getElementsByTagName("h3");
 // console.log(h3[0]);
@@ -33,7 +35,66 @@
 // h3[0].innerHTML = '<div><h3>Hello, Minh !</h3></div>';
 // h3[0].innerText = 'Hello, Minh !';
 
-for (let i = 0; i < h3.length; i++) {
-    h3[i].innerText = 'Hello, Minh !' + i;
-    console.log(h3[i]);
-}
+// for (let i = 0; i < h3.length; i++) {
+//     h3[i].innerText = 'Hello, Minh !' + i;
+//     console.log(h3[i]);
+// }
+
+
+// vòng lặp
+// var title = document.getElementsByTagName('h3');
+// console.log(title);
+
+// for (let i = 0; i < title.length; i++) {
+//     title[i].innerHTML = 'Hello, Minh!' + 'time ' + i;
+// }
+
+// document.getElementById('[id]')
+// var element = document.getElementById('card-1');
+// console.log(element);
+
+// document.getElementsByClassName('[class]') -> trả về mảng -> thay đổi từng phần tử trong mảng
+// var element = document.getElementsByClassName('card-title');
+// console.log(element);
+// for (let i = 0; i < element.length; i++) {
+//     element[i].innerHTML = 'Title have changed'
+// }
+
+// document.querySelector("[data as CSS]" &  document.querySelector("[data as CSS]"
+// var element = document.querySelector('.card-title');
+// console.log(element);
+
+// var element_all = document.querySelectorAll('.card-title');
+// console.log(element_all);
+// console.log(element_all[1]);
+// for (let i = 0; i < element_all.length; i++) {
+//     console.log(element_all[i]);
+
+//     if (i == 2){
+//         element_all[i].innerHTML = 'Title has selected';
+//     }
+// }
+
+// var element_id = document.querySelectorAll('#card-1');
+// console.log(element_id);
+
+// var element_data = document.querySelectorAll('[data-note]');
+// console.log(element_data);
+
+// classList.remove('[class]')
+// classList.add('[class]')
+// classList.toggle('[class]')
+// var element = document.querySelectorAll('.card .action');
+// console.log(element);
+// element[0].classList.remove('action');
+// element[0].classList.add('btn');
+
+// element[1].classList.toggle('change-class');
+
+// xử lý sự kiện
+// document.addEventListener('DOMContentLoaded', function(){
+//     var x1 = document.querySelectorAll('.action');
+//     x1[0].onclick = function(){
+//         x1[0].classList.toggle('btn');
+//     }
+// }, false)
