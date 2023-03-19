f4f3544fbccfbed1bff36b4ee80876c42f28dcfa
faker <118911265+loveyou209@users.noreply.github.com>
2023-03-19 18:25:17 +0700

    Form in React.Js

--- reactbasic/src/views/Example/MyComponent.js
+++ reactbasic/src/views/Example/MyComponent.js
@@ -1,9 +1,48 @@
 import React from "react";
 
 class MyComponent extends React.Component{
+    state = {
+        firstName: '',
+        lastName: ''
+    }
+
+    handleChangeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('>>> check submit form: ', this.state);
+    }
+
     render(){
+        console.log('>>> call render: ', this.state);
         return (
-            <div>Hello My Component</div>
+            <React.Fragment>
+                <form action="/action_page.php">
+                    <label htmlFor="fname">First name:</label><br/>
+                    <input 
+                        type="text" 
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleChangeFirstName(event)}
+                    /><br/>
+                    <label htmlFor="lname">Last name:</label><br/>
+                    <input 
+                    type="text" 
+                    value={this.state.lastName}
+                    onChange={(event) => this.handleChangeLastName(event)}
+                    /><br/><br/>
+                    <input type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)}/>
+                </form> 
+            </React.Fragment>
         )
     }
 }
